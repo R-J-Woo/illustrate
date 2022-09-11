d7ade6661da0ee390784297fb384a47b7e75bf21
Jewoo Ryu <jewoo20000624@gmail.com>
2022-09-11 09:53:57 +0900

    일러스트 목록 필터링 기능 추가

--- illustrates/views.py
+++ illustrates/views.py
@@ -4,6 +4,7 @@ from .models import Illustrates
 from .serializers import IllustCreateSerializer, IllustSerializer
 from .permissions import CustomReadOnly
 from users.models import Profile
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 
@@ -11,6 +12,8 @@ from users.models import Profile
 class IllustViewSet(viewsets.ModelViewSet):
     queryset = Illustrates.objects.all()
     permission_classes = [CustomReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['illustrator', 'likes']
 
     def get_serializer_class(self):
         if self.action == 'list' or 'retrieve':
